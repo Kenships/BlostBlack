3e03859312f112d2583ec1aaf64cb8f1c0516bd0
Kenneth <kenships0402@gmail.com>
2024-12-31 18:31:18 -0500

    Cell hover select working note: added empty methods for Emily to fill

--- Assets/Scripts/GridCellManager.cs
+++ Assets/Scripts/GridCellManager.cs
@@ -1,29 +1,76 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class GridCellManager : MonoBehaviour
 {
     private const int GRID_SIZE = 8 ;
-    [SerializeField] private List<GameObject> gridHolder;
-    [SerializeField] private GameObject gridVisual;
-    [SerializeField] private GameObject grid;
-    private GameObject[][] _gridCellLocations;
-    private Vector3 _firstCellLocation;
+    [SerializeField] private GameObject gridVisual; 
+    [SerializeField] private Grid gridObject;
+    private bool [][] _gridOccupancy;
+
+    private void Awake()
+    {
+        InitializeGridOccupancy();
+    }
     private void Start()
     {
-        _gridCellLocations = new GameObject[GRID_SIZE][];
-        for (int row = 0; row < GRID_SIZE; row ++)
-        {
-            _gridCellLocations[row] = new GameObject[GRID_SIZE];
-            for (int col = 0; col < GRID_SIZE; col++)
-            {
-                _gridCellLocations[row][col] = gridHolder[row * GRID_SIZE + col];
-            }
-                
-        }
+        InputManager.Instance.OnConfirm += InputOnConfirm;
+    }
 
-        _firstCellLocation = grid.transform.position;
-        Debug.Log(_firstCellLocation);
-        gridVisual.transform.position = _firstCellLocation;
-    }   
+    private void Update()
+    {
+        UpdateHoverCellPosition();
+    }
+    
+    
+    private void InputOnConfirm(object sender, EventArgs e)
+    {
+        OutputHoverGridCoordinates();
+        OccupyPosition(GetHoverCellLocalPosition());
+    }
+    
+    private void UpdateHoverCellPosition()
+    {
+        //Need to check bounds of input
+        gridVisual.transform.position = GetHoverCellWorldPosition();
+    }
+
+    private void OccupyPosition(Vector3 position)
+    {
+        
+    }
+
+    private bool IsCellOccupied(Vector3 coordinates)
+    {
+        return false;
+    }
+
+    private Vector3 GetHoverCellLocalPosition()
+    {
+        return GetLocalizedGridCoordinates(GetHoverCellWorldPosition());
+    }
+    private Vector3 GetHoverCellWorldPosition()
+    {
+        return gridObject.CellToWorld(gridObject.WorldToCell(InputManager.Instance.GetPointerPosition()));
+    }
+    
+    private Vector3 GetLocalizedGridCoordinates(Vector3 worldGridCoordinate)
+    {
+        Vector3 unitVector = new Vector3(1,1,0);
+        return worldGridCoordinate + (GRID_SIZE/2 * unitVector);
+    }
+
+    private void InitializeGridOccupancy()
+    {
+        
+    }
+
+    private void OutputHoverGridCoordinates()
+    {
+        Vector3Int worldGridCoordinate = gridObject.WorldToCell(InputManager.Instance.GetPointerPosition());
+        Debug.Log("World grid coordinates " + worldGridCoordinate);
+        Debug.Log("Local grid coordinate " + GetLocalizedGridCoordinates(worldGridCoordinate));
+    }
 }
--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -1,12 +1,48 @@
+using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class InputManager : MonoBehaviour
 {
     [SerializeField] private GameObject pointerVisual;
+    public static InputManager Instance { get; private set; }
+    public event EventHandler OnConfirm;
+    InputSystem_Actions inputSystem_Actions;
+    
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            Instance = this;
+        }
+        inputSystem_Actions = new InputSystem_Actions();
+        inputSystem_Actions.Enable();
+    }
+
+    private void Start()
+    {
+        inputSystem_Actions.Player.Confirm.performed += ConfirmOnPerformed;
+    }
+
+    private void ConfirmOnPerformed(InputAction.CallbackContext obj)
+    {
+        OnConfirm?.Invoke(this, EventArgs.Empty);
+    }
 
     private void Update()
     {
         Vector3 pointerPosition = Input.mousePosition;
         pointerVisual.transform.position = pointerPosition;
     }
+
+    public Vector3 GetPointerPosition()
+    {
+        return Input.mousePosition;
+    }
+    
+    
 }
